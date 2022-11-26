cb06b153cd175d17c547d7153bcecaef74d7b042
Derek M Berger <dmberger.dev@gmail.com>
2022-11-25 23:32:58 -0500

    path hacks

--- src/enumerables.py
+++ src/enumerables.py
@@ -1,3 +1,13 @@
+from __future__ import annotations
+
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent.parent  # isort: skip
+sys.path.append(str(ROOT))  # isort: skip
+# fmt: on
+
+
 from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass
 from enum import Enum
--- src/metrics.py
+++ src/metrics.py
@@ -1,3 +1,13 @@
+from __future__ import annotations
+
+# fmt: off
+import sys  # isort: skip
+from pathlib import Path  # isort: skip
+ROOT = Path(__file__).resolve().parent.parent  # isort: skip
+sys.path.append(str(ROOT))  # isort: skip
+# fmt: on
+
+
 from typing import Any, Callable, Mapping, Type, Union, cast, no_type_check
 
 import torch
